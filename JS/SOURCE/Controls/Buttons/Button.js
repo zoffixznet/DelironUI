7776f5fb331434c048233a7d0668f1a458930015
Zoffix Znet <cpan@zoffix.com>
2014-01-16 12:48:43 -0500

    Made 'Plain Button' work

--- JS/SOURCE/Controls/Buttons/Button.js
+++ JS/SOURCE/Controls/Buttons/Button.js
@@ -8,17 +8,43 @@ Deliron_UI_Contol_Button.prototype.construct = function (modifier) {
     if ( ! modifier ) { modifier = '' }
 
     left   = new Element('span', {
-        'class': 'dui_control_button_left ' + modifier
+        'class': 'dui_b_l ' + modifier
     });
     middle = new Element('span', {
-        'class': 'dui_control_button_middle ' + modifier
+        'class': 'dui_b_m ' + modifier
     });
     right  = new Element('span', {
-        'class': 'dui_control_button_right '  + modifier
+        'class': 'dui_b_r '  + modifier
     });
 
-    middle.replaces(this._el);
-    left.inject(middle);
+    left.addEvent(  'mouseover', function(){this.addClass('dui_hover')} );
+    // middle.addEvent('mouseover', function(){this.addClass('dui_hover')} );
+    // right.addEvent( 'mouseover', function(){this.addClass('dui_hover')} );
+    // this._el.addEvent('mouseover', function(){this.addClass('dui_hover')} );
+    left.addEvent(  'mouseout', function(){this.removeClass('dui_hover')} );
+    // middle.addEvent('mouseout', function(){this.removeClass('dui_hover')} );
+    // right.addEvent( 'mouseout', function(){this.removeClass('dui_hover')} );
+    // this._el.addEvent( 'mouseout', function(){this.removeClass('dui_hover')} );
+
+
+    left.addEvent('click', function(){
+        alert(this);
+        this.getElement('.dui_b_orig').click();
+        return false;
+    });
+    right.addEvent('click', function(){
+        this.getElement('.dui_b_orig').click();
+        return false;
+    });
+    middle.addEvent('click', function(){
+        this.getElement('.dui_b_orig').click();
+        return false;
+    });
+
+    left.replaces(this._el);
     right.inject(left);
-    this._el.inject(right);
+    middle.inject(right);
+    this._el.inject(middle);
+    this._el.addClass('dui_b_orig');
+    this._el.onclick = function() { alert("Clicked!"); }
 }
