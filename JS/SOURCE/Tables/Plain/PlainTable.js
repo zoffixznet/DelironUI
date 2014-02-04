ad5e3c8dccecfaf91881d2e7b3969e8e173c6c01
Zoffix Znet <cpan@zoffix.com>
2014-02-04 10:24:30 -0500

    Added .plain.table fixes #7

--- JS/SOURCE/Tables/Plain/PlainTable.js
+++ JS/SOURCE/Tables/Plain/PlainTable.js
@@ -5,7 +5,7 @@ Deliron_UI_Table_Plain.prototype.init = function () {
     var els = $$('.dui.plain.table');
 
     els.each(function (el) {
-        [ 'plain', 'table'].each(function (className) {
+        [ 'plain', 'table' ].each(function (className) {
             el.removeClass(className);
         });
         new Deliron_UI_Table(el).construct('dui_table_plain');
